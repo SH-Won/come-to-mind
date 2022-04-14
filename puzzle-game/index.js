4c69192c82e805d66c1b6f14ab9c5c0c3a1cebd1
SH-Won <1317good@naver.com>
2022-04-14 16:51:21 +0900

    puzzle game drag drop

--- puzzle-game/index.js
+++ puzzle-game/index.js
@@ -4,6 +4,7 @@ class Game{
         this.size = size;
         this.tiles = null;
         this.dragged = {
+            el:null,
             index:null,
             class:null,
         }
@@ -12,46 +13,76 @@ class Game{
         const playTime = document.createElement('div');
         const startBtn = document.createElement('button');
         this.tiles = this.createTiles(this.size);
-        console.log(this.tiles);
-        this.tiles.forEach(li => this.$target.appendChild(li))
-        const div = document.createElement('div');
-        // div.addEventListener('dragstart')
+        this.tiles.forEach(li => this.$target.appendChild(li));
+        
+        setTimeout(()=>{
+            this.$target.innerHTML = '';
+            this.suffle(this.tiles).forEach(tile => this.$target.appendChild(tile));
+
+        },2000);
+
         this.$target.addEventListener('dragstart', e =>{
             if(e.target.tagName !== 'LI') return;
             const object = e.target;
+            this.dragged.el = object;
+            this.dragged.index = [...object.parentNode.children].indexOf(object);
+            this.dragged.class = object.className;
+        })
+        this.$target.addEventListener('dragover',e =>{
+            e.preventDefault();
+        })
+        this.$target.addEventListener('drop', e=>{
+            if(e.target.tagName !=='LI') return;
+            const object = e.target;
+            if(object.className !== this.dragged.class){
+                let originPlace , isLast ;
+                if(this.dragged.el.nextSibling){
+                    originPlace = this.dragged.el.nextSibling;
+                    isLast = false;
+                }else{
+                    originPlace = this.dragged.el.previousSibling;
+                    isLast = true;
+                }
+                const dropIndex = [...object.parentNode.children].indexOf(object);
+                this.dragged.index > dropIndex ? object.before(this.dragged.el) : object.after(this.dragged.el);
+                isLast ? originPlace.after(object) : originPlace.before(object);
+            }
+            this.checkStatus();
         })
     }
     createTiles = (size) => {
-        console.log(window.screenX);
-        console.dir(this.$target)
         const width = this.$target.offsetWidth / size;
-        console.log(width);
         const tiles = Array(size*size).fill().map((_,i) =>{
             const li = document.createElement('li');
-            const img = document.createElement('img');
-            img.src = '../images/dc.jpg';
             const [dy,dx] = [Math.floor(i/size), i%size];
-            img.style.top = `-${width*dy}px`;
-            img.style.left = `-${width*dx}px`;
-            img.style.width = `${100*size}%`;
-            img.style.height = `${100*size}%`;
-            li.appendChild(img);
             li.className = `li${i}`;
             li.setAttribute('data-index',i);
+            li.setAttribute('draggable',true);
             li.style.width = `${width}px`;
             li.style.height = `${width}px`;
             li.style.border = `0.5px solid white`;
+            li.style.background = `top -${width*dy}px left -${width*dx}px / ${this.$target.offsetWidth}px ${this.$target.offsetWidth}px url("../images/dc.jpg")`;
             return li;
         });
-        // tiles.forEach((tile,i)=>{
-        //     
-        //     tile.style.backgroundPositionY =`-${width*dy}px`
-        //     tile.style.backgroundPositionX =`-${width*dx}px`
-        // })
         return tiles;
     }
     suffle = (tiles) =>{
-
+        let i = tiles.length - 1;
+        while(i > 0){
+            const random = Math.floor(Math.random() * (i+1));
+            [tiles[i],tiles[random]] = [tiles[random],tiles[i]];
+            i--; 
+        }
+        return tiles;
+    }
+    checkStatus = () =>{
+        const isMatch = [...this.$target.children].every((el,i) => el.dataset.index === i.toString() );
+        // console.log(this.dragged.el.dataset);
+        if(isMatch){
+            console.log('match');
+        }else{
+            console.log('unMatch');
+        }
     }
 }
 const init = () =>{
@@ -59,7 +90,8 @@ const init = () =>{
     const root = document.querySelector('#root');
     const container = document.createElement('ul');
     container.className = 'container';
-    container.style.width = `${document.documentElement.scrollWidth * 0.7}px`
+    container.style.width = `${document.documentElement.scrollWidth * 0.7}px`;
+    container.style.height = `${document.documentElement.scrollWidth * 0.7}px`;
     container.style.gridTemplateColumns = `repeat(${size},1fr)`
     root.appendChild(container);
     new Game(container,size).init();
