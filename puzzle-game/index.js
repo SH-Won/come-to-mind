5cdf8e93ef40f412be5fdd374ff5fa424c9cfcf1
SH-Won <1317good@naver.com>
2022-04-14 19:53:13 +0900

    puzzle game start timer

--- puzzle-game/index.js
+++ puzzle-game/index.js
@@ -3,6 +3,14 @@ class Game{
         this.$target = $target;
         this.size = size;
         this.tiles = null;
+        this.container = null;
+        this.isPlaying = false;
+        this.time = 0;
+        this.timeInterval = null;
+        this.gameText = document.createElement('span');
+        this.playTime = document.createElement('div');
+        this.startBtn = document.createElement('button');
+        this.initialPlay = false;
         this.dragged = {
             el:null,
             index:null,
@@ -10,19 +18,36 @@ class Game{
         }
     }
     init = () =>{
-        const playTime = document.createElement('div');
-        const startBtn = document.createElement('button');
-        this.tiles = this.createTiles(this.size);
-        this.tiles.forEach(li => this.$target.appendChild(li));
-        
-        setTimeout(()=>{
-            this.$target.innerHTML = '';
-            this.suffle(this.tiles).forEach(tile => this.$target.appendChild(tile));
-
-        },2000);
+        const containerSize = document.documentElement.scrollWidth * 0.7
+        if(!this.initialPlay){
+        const container = document.createElement('ul');
+        container.className = 'container';
+        container.style.width = `${containerSize}px`;
+        container.style.height = `${containerSize}px`;
+        container.style.gridTemplateColumns = `repeat(${this.size},1fr)`
+        this.container = container;
+        this.gameText.className = 'gameText';
+        this.playTime.className = 'playTime';
+        this.startBtn.className = 'startBtn';
+        this.startBtn.innerText = 'START';
+        this.gameText.innerText = 'SUCCESS PUZZLE'
+        this.gameText.style.display = 'none';
+        this.$target.appendChild(this.playTime);
+        this.$target.appendChild(this.container);
+        this.$target.appendChild(this.startBtn);
+        this.$target.appendChild(this.gameText);
+        this.tiles = this.createTiles(containerSize , this.size);
+        this.playTime.innerText = this.time;
+        }
+        else{
+            this.tiles.sort((a,b) => parseInt(a.dataset.index) - parseInt(b.dataset.index));
+            this.playTime.innerText = this.time;
+        }
+        this.container.innerHTML = '';
+        this.tiles.forEach(tile => this.container.appendChild(tile));
 
         this.$target.addEventListener('dragstart', e =>{
-            if(e.target.tagName !== 'LI') return;
+            if(e.target.tagName !== 'LI' || !this.isPlaying) return;
             const object = e.target;
             this.dragged.el = object;
             this.dragged.index = [...object.parentNode.children].indexOf(object);
@@ -32,7 +57,7 @@ class Game{
             e.preventDefault();
         })
         this.$target.addEventListener('drop', e=>{
-            if(e.target.tagName !=='LI') return;
+            if(e.target.tagName !=='LI' || !this.isPlaying) return;
             const object = e.target;
             if(object.className !== this.dragged.class){
                 let originPlace , isLast ;
@@ -49,19 +74,36 @@ class Game{
             }
             this.checkStatus();
         })
+        this.$target.addEventListener('click',e =>{
+            if(e.target.className !== 'startBtn' || this.isPlaying) return;
+            this.start();
+        })
     }
-    createTiles = (size) => {
-        const width = this.$target.offsetWidth / size;
-        const tiles = Array(size*size).fill().map((_,i) =>{
+    start = () =>{
+        this.gameText.style.display = 'none';
+        setTimeout(()=>{
+            // clearInterval(timeInterval);
+            this.suffle(this.tiles).forEach(tile => this.container.appendChild(tile));
+            this.isPlaying = true;
+            this.timeInterval = setInterval(()=>{
+                 this.time++;
+                 this.playTime.innerText = this.time;
+            },1000)
+
+        },2000);
+    }
+    createTiles = (containerSize,tileSize) => {
+        const width = containerSize / tileSize;
+        const tiles = Array(tileSize*tileSize).fill().map((_,i) =>{
             const li = document.createElement('li');
-            const [dy,dx] = [Math.floor(i/size), i%size];
+            const [dy,dx] = [Math.floor(i/tileSize), i%tileSize];
             li.className = `li${i}`;
             li.setAttribute('data-index',i);
             li.setAttribute('draggable',true);
             li.style.width = `${width}px`;
             li.style.height = `${width}px`;
             li.style.border = `0.5px solid white`;
-            li.style.background = `top -${width*dy}px left -${width*dx}px / ${this.$target.offsetWidth}px ${this.$target.offsetWidth}px url("../images/dc.jpg")`;
+            li.style.background = `top -${width*dy}px left -${width*dx}px / ${containerSize}px ${containerSize}px url("insert your image url")`;
             return li;
         });
         return tiles;
@@ -76,24 +118,22 @@ class Game{
         return tiles;
     }
     checkStatus = () =>{
-        const isMatch = [...this.$target.children].every((el,i) => el.dataset.index === i.toString() );
-        // console.log(this.dragged.el.dataset);
+        const isMatch = [...document.querySelector('.container').children].every((el,i) => el.dataset.index === i.toString() );
         if(isMatch){
-            console.log('match');
+            clearInterval(this.timeInterval);
+            this.time = 0;
+            this.isPlaying = false;
+            this.initialPlay = true;
+            this.playTime.innerText = this.time;
+            this.gameText.style.display = 'block';
         }else{
-            console.log('unMatch');
+            
         }
     }
 }
 const init = () =>{
-    const size = 5;
+    const size = 5; // you can choose puzzle matrix size (ex. size * size matrix);
     const root = document.querySelector('#root');
-    const container = document.createElement('ul');
-    container.className = 'container';
-    container.style.width = `${document.documentElement.scrollWidth * 0.7}px`;
-    container.style.height = `${document.documentElement.scrollWidth * 0.7}px`;
-    container.style.gridTemplateColumns = `repeat(${size},1fr)`
-    root.appendChild(container);
-    new Game(container,size).init();
+    new Game(root,size).init();
 }
 init();
