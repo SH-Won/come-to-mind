5c82e79b54611ac47fa5d7064250118901d6e01d
SH-Won <1317good@naver.com>
2022-04-17 23:11:02 +0900

    drop block faster & clear line

--- tetris/index.js
+++ tetris/index.js
@@ -12,23 +12,24 @@ class Tetris{
         this.playGround.className= 'playGround';
         this.playGround.style.width = `${this.$target.offsetWidth*0.5}px`;
         const {playGround,ROW} = this;
-        const height = this.$target.offsetWidth*0.5 / this.COL;
+
         for(let i=0; i<ROW; i++){
-            const li = document.createElement('li');
-            li.style.height = `${height}px`;
-            li.appendChild( this.prependLine() );
-            playGround.prepend(li);
+            playGround.prepend( this.prependLine());
         }
         this.event();
         this.createBlock();
     }
     prependLine = () =>{
         const {COL} = this;
-        const row = document.createElement('ul');
+        const height = this.$target.offsetWidth*0.5 / this.COL;
+        const row = document.createElement('li');
+        row.style.height = `${height}px`;
+        const ul = document.createElement('ul');
         for(let i=0; i<COL; i++){
             const li = document.createElement('li');
-            row.appendChild(li);
+            ul.appendChild(li);
         }
+        row.prepend(ul);
         return row;
     }
     renderBlock = (moveType=null) =>{
@@ -38,10 +39,10 @@ class Tetris{
         this.BLOCKS[type][direction].some(([y,x])=>{
             const [ny,nx] = [y+top,x+left];
             const target = this.playGround.childNodes[ny] ? this.playGround.childNodes[ny].children[0].childNodes[nx] : null;
-            if(ny < 0 || nx < 0 || ny >= this.ROW || nx >= this.COL || target.classList.contains('seize')){
+            if(!target || target.classList.contains('seize')){
                 this.tempMovingBlock = {...this.movingBlock};
                 setTimeout(()=>{
-                    this.renderBlock();
+                    this.renderBlock('re');
                     if(moveType ==='top'){
                         this.seizeBlock();
                     }
@@ -56,6 +57,19 @@ class Tetris{
     seizeBlock = () =>{
         const currentBlock = document.querySelectorAll('.moving');
         currentBlock.forEach(block => (block.classList.remove('moving'), block.classList.add('seize')))
+        this.checkLine();
+    }
+    checkLine = () =>{
+        const {type,direction,top} = this.movingBlock;
+        const rowPos = Array.from(new Set(this.BLOCKS[type][direction].map(([y,x]) => y+top))).sort((a,b) => a-b);
+        // console.log(rowPos)
+        // console.log(this.playGround.childNodes[14].children[0].childNodes);
+        rowPos.forEach(y =>{
+            if([...this.playGround.childNodes[y].children[0].childNodes].every(child => child.classList.contains('seize'))){
+                this.playGround.childNodes[y].remove();
+                this.playGround.prepend(this.prependLine());
+            }
+        })
         this.createBlock();
     }
     createBlock = () =>{
@@ -83,13 +97,21 @@ class Tetris{
         this.tempMovingBlock.direction = direction + 1 >= this.BLOCKS[type].length ? 0 : direction + 1;
         this.renderBlock();
     }
+    dropBlock = () =>{
+        clearInterval(this.interval);
+        this.interval = setInterval(() =>{
+             this.moveBlock('top',1);
+        },10)
+    }
     event = () =>{
         document.addEventListener('keydown',e =>{
+            console.log(e.keyCode);
             switch(e.keyCode){
                 case 37 : return this.moveBlock('left',-1);
                 case 39 : return this.moveBlock('left',1);
                 case 40 : return this.moveBlock('top',1);
                 case 38 : return this.rotateBlock();
+                case 32 : return this.dropBlock();
                 default : break;
             }
         })
